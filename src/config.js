6e879efd22d0a3be45fe703ae236889fb99df0e5
Bill Yu <han.yu@duke.edu>
2018-06-10 17:14:20 +0800

    set max body size to get rid of 413

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 const siteURL = process.env.NODE_ENV === 'production' ? 'http://vcm-3422.vm.duke.edu' : 'http://localhost';
+const postFix = process.env.NODE_ENV === 'production' ? '/api' : ':1717';
 
 module.exports = {
   blogName: 'Bill Yu\'s Blog',
   githubLink: 'https://github.com/billhanyu/',
   pageCount: 2,
   paginationDisplayCount: 5,
-  baseURL: `${siteURL}/api`,
+  baseURL: `${siteURL}${postFix}`,
   siteURL,
 };
